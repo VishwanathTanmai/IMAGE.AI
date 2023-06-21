2697dc04f72b61dfaa720714020fdde058c9cf80
vishwanath tanmai <122545477+VishwanathTanmai@users.noreply.github.com>
2023-06-22 01:53:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const accessKey = "qocQWN_Cr-3RV9R-VmA9by20uW5mLI2_Nn2scWXr4Ps";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function seachImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=30`;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+
+   
+
+    const results = data.results;
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+
+
+
+}
+
+searchForm.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    page= 1;
+    seachImages();
+})
+
